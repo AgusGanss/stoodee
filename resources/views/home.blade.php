7fded2ba0ded5cafbd16ac892b8c1889085fb76d
agus <agusputraa47@gmail.com>
2024-07-08 17:38:22 +0800

    stoodee

---
+++ resources/views/home.blade.php
@@ -0,0 +1,344 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../css/swiper-bundle.min.css" />
+        <link rel="stylesheet" href="../css/home.css">
+    
+        <title>Stoodee</title>
+    </head>
+    <body>
+        {{-- HEADER --}}
+        <div class="navbar">
+            <div class="navbar-content">
+                <a href="#"><img src="" alt="STOODEE logos"></a>
+            </div>
+        </div>
+
+        <div class="judul-header">
+            <h1>Stoodee Bimbingan Belajar</h1>
+            <p>LETS STUDY FOR YOUR BRIGHTER FUTURE</p>
+        </div>
+
+        <div class="img-header">
+            <img src="../foto/foto.png" alt="">
+        </div>
+          {{-- HEADER --}}
+
+           {{-- BODY --}}
+           {{-- MENGAPA MEMILIH STOODEE --}}
+           <div class="judul-content-satu">
+            <h1>MENGAPA MEMILIH STOODEE???</h1>
+           </div>
+           <div class="content-satu">
+            <div class="card">
+                <h1>EKSTRA TUTOR ONLINE (EKSTO)</h1>
+                <p>Siswa yang mengalami kendala belajar
+                    dapat berkonsultasi melalui chat langsung 
+                    via WhatsApp dengan tutor sesuai dengan
+                    bidang studinya</p>
+            </div>
+            <div class="card">
+                <h1>EKSTRA TUTOR ONLINE (EKSTO)</h1>
+                <p>Siswa yang mengalami kendala belajar
+                    dapat berkonsultasi melalui chat langsung 
+                    via WhatsApp dengan tutor sesuai dengan
+                    bidang studinya</p>
+            </div>
+            <div class="card">
+                <h1>EKSTRA TUTOR ONLINE (EKSTO)</h1>
+                <p>Siswa yang mengalami kendala belajar
+                    dapat berkonsultasi melalui chat langsung 
+                    via WhatsApp dengan tutor sesuai dengan
+                    bidang studinya</p>
+            </div>
+            <div class="card">
+                <h1>EKSTRA TUTOR ONLINE (EKSTO)</h1>
+                <p>Siswa yang mengalami kendala belajar
+                    dapat berkonsultasi melalui chat langsung 
+                    via WhatsApp dengan tutor sesuai dengan
+                    bidang studinya</p>
+            </div>
+            <div class="card">
+                <h1>EKSTRA TUTOR ONLINE (EKSTO)</h1>
+                <p>Siswa yang mengalami kendala belajar
+                    dapat berkonsultasi melalui chat langsung 
+                    via WhatsApp dengan tutor sesuai dengan
+                    bidang studinya</p>
+            </div>
+            
+           </div>
+
+
+
+
+           {{-- PROGRAM--}}
+           <div class="judul-content-dua">
+            <h1>PROGRAM</h1>
+           </div>
+           <div class="content-dua">
+            <div class="card">
+                <img src="../foto/gpu.png" alt="Foto">
+                <div class="content-card">
+                    <h3>Judul</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis qui voluptate aspernatur eum, rerum necessitatibus dolor provident, maiores beatae facilis dolores, ad ducimus ea eaque atque voluptatibus saepe deserunt?</p>
+                <a href="#">Informasi</a>
+                </div>
+                
+            </div>
+            <div class="card">
+                <img src="../foto/gpu.png" alt="Foto">
+                <div class="content-card">
+                    <h3>Judul</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis qui voluptate aspernatur eum, rerum necessitatibus dolor provident, maiores beatae facilis dolores, ad ducimus ea eaque atque voluptatibus saepe deserunt?</p>
+                <a href="#">Informasi</a>
+                </div>
+                
+            </div>
+            <div class="card">
+                <img src="../foto/gpu.png" alt="Foto">
+                <div class="content-card">
+                    <h3>Judul</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis qui voluptate aspernatur eum, rerum necessitatibus dolor provident, maiores beatae facilis dolores, ad ducimus ea eaque atque voluptatibus saepe deserunt?</p>
+                <a href="#">Informasi</a>
+                </div>
+                
+            </div>
+            <div class="card">
+                <img src="../foto/gpu.png" alt="Foto">
+                <div class="content-card">
+                    <h3>Judul</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis qui voluptate aspernatur eum, rerum necessitatibus dolor provident, maiores beatae facilis dolores, ad ducimus ea eaque atque voluptatibus saepe deserunt?</p>
+                <a href="#">Informasi</a>
+                </div>
+                
+            </div>
+            <div class="card">
+                <img src="../foto/gpu.png" alt="Foto">
+                <div class="content-card">
+                    <h3>Judul</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis qui voluptate aspernatur eum, rerum necessitatibus dolor provident, maiores beatae facilis dolores, ad ducimus ea eaque atque voluptatibus saepe deserunt?</p>
+                <a href="#">Informasi</a>
+                </div>
+                
+            </div>
+            <div class="card">
+                <img src="../foto/gpu.png" alt="Foto">
+                <div class="content-card">
+                    <h3>Judul</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis qui voluptate aspernatur eum, rerum necessitatibus dolor provident, maiores beatae facilis dolores, ad ducimus ea eaque atque voluptatibus saepe deserunt?</p>
+                <a href="#">Informasi</a>
+                </div>
+                
+            </div>
+           </div>
+
+
+
+
+           {{-- REVIEW --}}
+           <div class="review">
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <div class="testimonialBox">
+                        <img src="../foto/gpu.png" alt="Foto Profile">
+                        <div class="content-testimoni">
+                            <h3 class="nama-testimoni">Nama</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa unde architecto quis quia perspiciatis laborum corporis maiores aliquid, quos magni dolore dolores. Molestiae facilis eveniet sit ab doloremque eligendi?</p>
+                        </div>
+                    </div>
+                    
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="testimonialBox">
+                        <img src="../foto/gpu.png" alt="Foto Profile">
+                        <div class="content-testimoni">
+                            <h3 class="nama-testimoni">Nama</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa unde architecto quis quia perspiciatis laborum corporis maiores aliquid, quos magni dolore dolores. Molestiae facilis eveniet sit ab doloremque eligendi?</p>
+                        </div>
+                    </div>
+                  </div>
+
+                  <div class="swiper-slide">
+                    <div class="testimonialBox">
+                        <img src="../foto/gpu.png" alt="Foto Profile">
+                        <div class="content-testimoni">
+                            <h3 class="nama-testimoni">Nama</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa unde architecto quis quia perspiciatis laborum corporis maiores aliquid, quos magni dolore dolores. Molestiae facilis eveniet sit ab doloremque eligendi?</p>
+                        </div>
+                    </div>
+                    
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="testimonialBox">
+                        <img src="../foto/gpu.png" alt="Foto Profile">
+                        <div class="content-testimoni">
+                            <h3 class="nama-testimoni">Nama</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa unde architecto quis quia perspiciatis laborum corporis maiores aliquid, quos magni dolore dolores. Molestiae facilis eveniet sit ab doloremque eligendi?</p>
+                        </div>
+                    </div>
+                  </div>
+
+                  <div class="swiper-slide">
+                    <div class="testimonialBox">
+                        <img src="../foto/gpu.png" alt="Foto Profile">
+                        <div class="content-testimoni">
+                            <h3 class="nama-testimoni">Nama</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa unde architecto quis quia perspiciatis laborum corporis maiores aliquid, quos magni dolore dolores. Molestiae facilis eveniet sit ab doloremque eligendi?</p>
+                        </div>
+                    </div>
+                    
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="testimonialBox">
+                        <img src="../foto/gpu.png" alt="Foto Profile">
+                        <div class="content-testimoni">
+                            <h3 class="nama-testimoni">Nama</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa unde architecto quis quia perspiciatis laborum corporis maiores aliquid, quos magni dolore dolores. Molestiae facilis eveniet sit ab doloremque eligendi?</p>
+                        </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+           </div>
+
+
+
+
+
+           {{-- NEWS --}}
+           <div class="news">
+            <div class="card-news">
+                <img src="../foto/gpu.png" alt="FOTO BERITA">
+                <div class="content-news">
+                    <div class="date">
+                        <img src="../foto/kalender.png" alt="icon" class="icon-tanggal">
+                         <p>Tanggal</p>
+                    </div>
+                    <div class="deskripsi">
+                        <h3>Judul</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus sunt vero iusto aut ad? Ut labore eos aliquam consequatur sunt laborum incidunt itaque soluta cumque impedit, ea commodi praesentium.</p>
+                    </div>
+                    <a href="#">LIHAT NEWS</a>
+                </div>
+            </div>
+            <div class="card-news">
+                <img src="../foto/gpu.png" alt="FOTO BERITA">
+                <div class="content-news">
+                    <div class="date">
+                        <img src="../foto/kalender.png" alt="icon" class="icon-tanggal">
+                         <p>Tanggal</p>
+                    </div>
+                    <div class="deskripsi">
+                        <h3>Judul</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus sunt vero iusto aut ad? Ut labore eos aliquam consequatur sunt laborum incidunt itaque soluta cumque impedit, ea commodi praesentium.</p>
+                    </div>
+                    <a href="#">LIHAT NEWS</a>
+                </div>
+            </div>
+            <div class="card-news">
+                <img src="../foto/gpu.png" alt="FOTO BERITA">
+                <div class="content-news">
+                    <div class="date">
+                        <img src="../foto/kalender.png" alt="icon" class="icon-tanggal">
+                         <p>Tanggal</p>
+                    </div>
+                    <div class="deskripsi">
+                        <h3>Judul</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus sunt vero iusto aut ad? Ut labore eos aliquam consequatur sunt laborum incidunt itaque soluta cumque impedit, ea commodi praesentium.</p>
+                    </div>
+                    <a href="#">LIHAT NEWS</a>
+                </div>
+            </div>
+           </div>
+
+
+
+           
+
+
+
+           {{-- ALAMAT --}}
+           <div class="alamat">
+            <div class="content-alamat">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.6197139068304!2d115.21843507477587!3d-8.63245729141388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd23f454587b641%3A0x229b7a63f796de36!2sStoodee%20Bimbel!5e0!3m2!1sid!2sid!4v1720335197301!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="card-alamat">
+                    <div class="deskripsi">
+                        <img src="../foto/home.png" alt="icon">
+                        <p>Jl. Gatot Subroto I No.32, Tonja, Kec. Denpasar Utara, Kota Denpasar, Bali 80236</p>
+                    </div>
+                    <div class="deskripsi">
+                        <img src="../foto/home.png" alt="icon">
+                        <p>Jl. Gatot Subroto I No.32, Tonja, Kec. Denpasar Utara, Kota Denpasar, Bali 80236</p>
+                    </div>   
+                    <div class="deskripsi">
+                        <img src="../foto/home.png" alt="icon">
+                        <p>Jl. Gatot Subroto I No.32, Tonja, Kec. Denpasar Utara, Kota Denpasar, Bali 80236</p>
+                    </div>
+                    <div class="deskripsi">
+                        <img src="../foto/home.png" alt="icon">
+                        <p>Jl. Gatot Subroto I No.32, Tonja, Kec. Denpasar Utara, Kota Denpasar, Bali 80236</p>
+                    </div>
+                    <div class="deskripsi">
+                        <img src="../foto/home.png" alt="icon">
+                        <p>Jl. Gatot Subroto I No.32, Tonja, Kec. Denpasar Utara, Kota Denpasar, Bali 80236</p>
+                    </div>
+
+                </div>
+            </div>
+           </div>
+
+
+           {{-- BODY --}}
+
+
+           {{-- FOOTER --}}
+           <div class="footer">
+                <div class="footer-content">
+                    <p>&copy; Copyright 2024 Stoodee,Created by Artha</p>
+                </div>
+           </div>
+           
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        <script>
+            window.addEventListener('scroll', function() {
+                var navbar = document.querySelector('.navbar');
+                navbar.classList.toggle('scrolled', window.scrollY > 50);
+            });
+        </script>
+          <script src="../js/swiper-bundle.min.js"></script>
+        <script>
+            var swiper = new Swiper(".mySwiper", {
+              effect: "coverflow",
+              grabCursor: true,
+              centeredSlides: true,
+              slidesPerView: "auto",
+              coverflowEffect: {
+                rotate: 0,
+                stretch:0,
+                depth: 100,
+                modifier: 2,
+                slideShadows: true,
+              },
+             loop: true,
+            });
+          </script>
+    </body>
+    </html>
